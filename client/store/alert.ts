2fff9ee9c809ca7c33e06ea6ee8863d9f713e72f
Adrian Malec <67909049+f10k1@users.noreply.github.com>
2023-07-30 15:38:56 +0200

    Update alert.ts

--- client/store/alert.ts
+++ client/store/alert.ts
@@ -11,15 +11,6 @@ export const useAlertStore = defineStore('alert', () => {
         return (input: string) => validationErrors.value[input] ?? null;
     });
 
-    const addMessage = (message: Messages, code: number) => {
-        if (code == 422) {
-            validationErrors.value = message;
-            return;
-        }
-
-        messages.value = message;
-    };
-
     const addAlert = (alert: string | string[]) => {
         if (typeof alert === "string") alerts.value.push(alert);
         else alerts.value = [...alerts.value, ...alert];
@@ -31,15 +22,35 @@ export const useAlertStore = defineStore('alert', () => {
         alerts.value.shift();
     };
 
+    const addMessage = (message: Messages, code: number) => {
+        if (code == 422) {
+            validationErrors.value = message;
+            return;
+        }
+
+        messages.value = message;
+    };
+
     const message = computed(() => {
         return (name: string) => messages.value[name];
     });
 
+    const removeMessage = (key: string) => {
+        delete messages.value[key];
+    };
+
+    const clearValidationMessages = () => {
+        validationErrors.value = {};
+    };
+
     return {
         validationErrors,
         validationError,
+        messages,
         addMessage,
         addAlert,
+        removeMessage,
+        clearValidationMessages,
         removeAlert,
         alert,
         message
