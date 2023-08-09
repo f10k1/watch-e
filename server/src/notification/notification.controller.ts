2f1b34863106d62f49995d296fdd7485b71830f2
Adrian Malec <67909049+f10k1@users.noreply.github.com>
2023-08-09 21:14:42 +0200

    (api) endpoint for retreving all notifications for user

--- server/src/notification/notification.controller.ts
+++ server/src/notification/notification.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, HttpCode, HttpException, HttpStatus, Post } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpException, HttpStatus, Post, Req } from "@nestjs/common";
 import { Public } from "src/metadata.guard";
 import { NotificationDto } from "./notification.dto";
 import { NotificationService } from "./notification.service";
+import { Request } from "express";
+import { Notification } from "./notification.entity";
+import { Account } from "src/user/user.entity";
 
 @Controller("notification")
 export class NotificationController {
@@ -18,5 +21,13 @@ export class NotificationController {
         catch {
             throw new HttpException("something went wrong", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Get("all")
+    async getAll(@Req() req: Request) {
+        const notifications: Notification[] = await this.notificationService.getAll((req.user as Account).id);
+
+        return notifications;
     }
 }
