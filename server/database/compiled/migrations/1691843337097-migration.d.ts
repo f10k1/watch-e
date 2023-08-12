3c81e2219b51a103a23d4b3c6964d7e1095762b7
Adrian Malec <67909049+f10k1@users.noreply.github.com>
2023-08-12 14:31:00 +0200

    (server) notification table update

---
+++ server/database/compiled/migrations/1691843337097-migration.d.ts
@@ -0,0 +1,6 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+export declare class Migration1691843337097 implements MigrationInterface {
+    name: string;
+    up(queryRunner: QueryRunner): Promise<void>;
+    down(queryRunner: QueryRunner): Promise<void>;
+}
--- server/src/notification/notification.entity.ts
+++ server/src/notification/notification.entity.ts
@@ -1,6 +1,6 @@
 import { IsEmail, Matches, Min } from "class-validator";
 import { Account } from "src/user/user.entity";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { NotificationTypes } from "./notification.types";
 
 @Entity()
@@ -26,6 +26,9 @@ export class Notification {
         default: false
     })
     seen: boolean;
+
+    @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" })
+    created_at: Date;
 
     @ManyToOne(() => Account, (account) => account.notifications)
     account: Account;
